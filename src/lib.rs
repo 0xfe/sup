f616c5c54f5f38e9fbbf44164b3896409d59e358
Mo <mohit@muthanna.com>
2023-08-25 22:04:44 -0400

    Fix broken lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,4 @@
-pub mod cpu;
 pub mod sample;
 pub mod series;
 pub mod util;
 pub mod window;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cpu::{cpu_usage, cpu_usage2};
-
-    #[test]
-    fn it_works() {
-        cpu_usage();
-        cpu_usage2();
-    }
-}
